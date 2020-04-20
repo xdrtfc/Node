a0c94a96f364ec31a954cbee371166e5293a8684
xdrtfc <52586530+xdrtfc@users.noreply.github.com>
2020-04-20 19:35:31 -0400

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,20 @@
+/* Andreas Hinsch
+ *
+ *  an478360
+ *
+ *  COP3505
+ *  
+ *  3/15/2020
+ */
+ 
+public class Node{
+
+int val;
+String Infinite;
+Node next, prev, up, down;
+
+public Node() {
+
+}
+
+}
